45ae18ba015c76d6111263e91e4fc6396ef5555f
Dario <dariocortizo@gmail.com>
2020-04-18 18:26:26 +0200

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -8,9 +8,9 @@ def main():
   	roll = random.randint(1, dice_size)
   	dice_sum += roll
   	if roll == 1:
-  		print(f'You rolled a {roll} Daaaamn!')
+  		print(f'You rolled a {roll} Critical Fail!')
   	elif roll == dice_size:
-  	 	print(f'You rolled a {roll} Daaaamn!')
+  	 	print(f'You rolled a {roll} Critical Success!')
   	else:
   		print(f'You rolled a {roll}')
 
